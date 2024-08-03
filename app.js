af4dd352702f49f2a38ba29bf794407be5ae8ae6
Franciscoas98 <famador98@gmail.com>
2024-08-02 23:46:55 -0600

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ function generarNumeroSecreto() {
 
     function condicionesIniciales() {
         asignarTextoElemento('h1','Juego del numero secreto!');
-        asignarTextoElemento('p',`Indica un numero del 1 al 10 ${numeroMaximo}`);
+        asignarTextoElemento('p',`Indica un numero del 1 al ${numeroMaximo}`);
         numeroSecreto = generarNumeroSecreto();
         intentos = 1;
 }
